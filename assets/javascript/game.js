3fbc858fafe0233c5d50998ed5d1b5e07b2cc0b4
SarahReyes <siwreyes@gmail.com>
2016-08-15 19:58:28 -0500

    wrote pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,28 @@
 
+$(‘<div class=“hi”>Hello World</div>’)
 
-//
+
+
+
+
+//computer picks and displays a random number between 19 - 120
+
+//4 buttons are displayed on the screen
+
+//4 buttons each have a random hidden value between 1 - 12
+
+//when the user clicks a button, add the value of the button to the user's sum
+
+//user's sum box increments by the value of the button, each time a button is clicked
+
+//user wins if their total sum is equal to the random computer pick
+
+//user continues to play if their sum is < the random computer pick
+
+//user loses if their sum is > the random computer pick
+
+//the game re-starts when the user wins or loses
+
+//if the user wins, display win++, if the user loses, display losses++
+
+//when the game re-starts, the computer picks a new random number between 19 - 120
