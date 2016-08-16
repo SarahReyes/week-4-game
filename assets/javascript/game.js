0726025d92fe84cc5c66e964a82c2998ea11a988
SarahReyes <siwreyes@gmail.com>
2016-08-16 18:08:30 -0500

    added an image JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,74 @@
 
+  var computerRandom = "";
+  var counter = 0;
+  var numbers = [10, 1, 7, 5];
+  var numberToGuess = 53;
+
+  //computer picks and displays a random number between 19 - 120
+  function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  console.log(getRandomInt(19, 120));
+
+  //4 buttons are displayed on the screen
 
 
-//computer picks and displays a random number between 19 - 120
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-console.log(getRandomInt(19, 120));
+$(document).ready(function() {
+  //4 buttons each have a random hidden value between 1 - 12
+  var image1 = $("<img>");
+  image1.attr("src", "assets/images/lego-blue.png");
+  image1.attr("click-num", numbers[0]);
+  image1.addClass("clickImage");
+  $('#image1').append(image1);
 
-//4 buttons are displayed on the screen
-
-
-//4 buttons each have a random hidden value between 1 - 12
-
-
-//when the user clicks a button, add the value of the button to the user's sum
-
-
-//user's sum box increments by the value of the button, each time a button is clicked
+  // var image1 = $("<img>");
+  // image1.attr("src", "assets/images/lego-blue.png");
+  // image1.attr("click-num", numbers[0]);
+  // image1.addClass("clickImage");
+  // $('#image1').append(image1);
 
 
-//user wins if their total sum is equal to the random computer pick
+  $('.clickImage').on('click', function(){
+        counter = counter + parseInt($(this).data('num'));
+
+        $('#yourNumber').text(counter);
+
+        if (counter == numberToGuess){
+          alert('You won!!!!');
+        }else if( counter > numberToGuess){
+          alert('You lost!');
+        }
+console.log(image1);
+  });
+});
+
+  //when the user clicks a button, add the value of the button to the user's sum
+  // $(document).ready(function() {
+  //     $("#btnSubmit").click(function(){
+  //         alert("button");
+  //     });
+  // });
+
+  //user's sum box increments by the value of the button, each time a button is clicked
 
 
-//user continues to play if their sum is < the random computer pick
+  //user wins if their total sum is equal to the random computer pick
 
 
-//user loses if their sum is > the random computer pick
+  //user continues to play if their sum is < the random computer pick
 
 
-//the game re-starts when the user wins or loses
+  //user loses if their sum is > the random computer pick
 
 
-//if the user wins, display win++, if the user loses, display losses++
+  //the game re-starts when the user wins or loses
 
 
-//when the game re-starts, the computer picks a new random number between 19 - 120
+  //if the user wins, display win++, if the user loses, display losses++
+
+
+  //when the game re-starts, the computer picks a new random number between 19 - 120
+
+
+
+
