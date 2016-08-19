b4488ce21ac4501f7ace507abbcc471566734d8e
SarahReyes <siwreyes@gmail.com>
2016-08-19 16:35:41 -0500

    adding the userGuess(es)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,68 @@
-//use single quotes '' when you're referencing something already in html
-//use double quotes "" when writing html in jQuery
-//Each crystal should have a random hidden value between 1 - 12
+
+// use single quotes '' when you're referencing something already in html
+// use double quotes "" when writing html in jQuery
+// Each lego should have a random hidden value between 1 - 12
 
 var counter = 0;
-var userNumString = "Your Number: ";
-// var numbers = [10, 7, 4, 1];
+var userGuess = 0;
 
-//computer picks a random number between 19 - 120
+// computer picks a random number between min, max
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+// random number picked for user to guess
 var computerRandom = "Number To Match: " + (getRandomInt(19, 120));
 console.log(computerRandom);
 
-//assign a number to each lego
+// assign a number to each lego
 var greenLegoNumber = getRandomInt(1, 12);
     console.log(greenLegoNumber);
-
 var blueLegoNumber = getRandomInt(1, 12);
     console.log(blueLegoNumber);
-
 var yellowLegoNumber = getRandomInt(1, 12);
     console.log(yellowLegoNumber);
-
 var redLegoNumber = getRandomInt(1, 12);
     console.log(redLegoNumber);
 
 $(document).ready(function() {
-    //display randomly picked computer number in html
+    // display randomly picked computer number in html
     $('.computer-number').text(computerRandom);
 
 
-
-
-     // user clicks on a lego and it returns the value
+    // user clicks on the green lego and it returns the value
     $('#green-lego').click(function() {
-        console.log(greenLegoNumber);
+        userGuess = userGuess + greenLegoNumber;
+        console.log(userGuess);
     });
+    // user clicks on the blue lego and it returns the value
     $('#blue-lego').click(function() {
-        console.log(blueLegoNumber);
+        userGuess = userGuess + blueLegoNumber;
+        console.log(userGuess);
     });
+    // user clicks on the yellow lego and it returns the value
     $('#yellow-lego').click(function() {
-        console.log(yellowLegoNumber);
+        userGuess = userGuess + yellowLegoNumber;
+        console.log(userGuess);
     });
+    // user clicks on the red lego and it returns the value
     $('#red-lego').click(function() {
-        console.log(redLegoNumber);
+        userGuess = userGuess + redLegoNumber;
+        console.log(userGuess);
     });
+
+
+
+
+    // display the user click
+    $('#user-sum').text();
+
 
 });
 
 
 
 
-  //user's sum box increments by the value of the button, each time a button is clicked
+    //user's sum box increments by the value of the lego, each time a lego is clicked
 
 
   //user wins if their total sum is equal to the random computer pick
